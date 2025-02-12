48625a7e987d895f9be1228bdb70f97b503a23fb
amirrezaebrahimpoor <amirrezaebrahimpoorcode@gmail.com>
2025-02-12 16:23:21 +0330

    excercise 567 added

--- course/urls.py
+++ course/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path('list/',views.lists)
-]
+    path('list/',views.lists),
+    path('detail/<id>',views.corse_detail,name='corse-detail'),
+    path('course_list',views.course_list),
+    path('param/<str:name>',views.param_name),
+    path('detail2/',views.detail2),
+    path('search-detail/<search>',views.search_detail)
+    ]
